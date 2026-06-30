b9d0faae730433ba3559c5d5d8ef69974341a42c
Yuying Zou <yzou0050@student.monash.edu>
2026-06-30 15:47:36 +0800

    Initial commit

---
+++ src/components/MyName.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="my-name">
+    <h2>Yuying Zou</h2>
+    <p>Student ID: 36668656</p>
+  </div>
+</template>
+
+<style scoped>
+.my-name {
+  text-align: center;
+  margin-top: 30px;
+  padding: 15px;
+}
+
+h2 {
+  color: #42b883;
+  margin-bottom: 8px;
+}
+
+p {
+  color: #666;
+  font-size: 16px;
+}
+</style>
